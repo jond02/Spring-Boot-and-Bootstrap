8d4cf9e20e794c5e0ca777e655bfc77d3cf0ec5d
Jonathan Dann <nwjon122@gmail.coml>
2016-08-30 20:42:52 -0700

    inital commit

---
+++ src/main/java/com/distantember/DistantemberApplication.java
@@ -0,0 +1,12 @@
+package com.distantember;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DistantemberApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DistantemberApplication.class, args);
+	}
+}
