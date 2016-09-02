97cbff3a15f0172a8ab346db0979fcde7be8cff7
Jonathan Dann <nwjon122@gmail.coml>
2016-09-02 13:20:38 -0700

    ajax test complete

---
+++ src/main/java/com/distantember/controllers/BootController.java
@@ -0,0 +1,26 @@
+package com.distantember.controllers;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Created by jondann on 9/2/16.
+ */
+
+@RestController
+public class BootController {
+
+    static int count = 0;
+
+    @RequestMapping("/boot")
+    public String boot(){
+
+        try {
+            Thread.sleep(1000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        return "Welcome to Spring in WA" + count++;
+    }
+}
